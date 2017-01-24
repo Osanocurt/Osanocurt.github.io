b085ccbbc56b669902c94da6a80cbd3533a862a3
Curtis Osano <osanocurt@hotmail.co.uk>
2017-01-24 11:34:25 +0000

    new styled website

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 console.log('winning');
+$(document).ready(function() {
+  $(function() {
+    $('body').on('click', 'a.scrollable', function(event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+      }, 2000, 'easeInOutExpo');
+      event.preventDefault();
+    });
+
+  });
+  $('.navbar-collapse ul li a').click(function(){
+    $('.navbar-collapse').collapse('hide');
+  });
+
+});
